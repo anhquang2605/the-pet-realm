680b3f33befa9fba180c9746fe3026391f23b4d8
anhquang2605 <anhquang2605@gmail.com>
2025-07-29 21:45:07 -0700

    removed root layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
-import RootLayout from "./../components/layout";
 import { AppProps } from "next/app";
 import './../pages/globals.css';
-import { Press_Start_2P } from 'next/font/google';
 
-const pressStart2P = Press_Start_2P({ weight: '400', subsets: ['latin'] });
-export default function App({ Component, pageProps } : AppProps) {
+export default function Document({ Component, pageProps } : AppProps) {
   return (
-    <RootLayout>
       <Component {...pageProps} />
-    </RootLayout>
   );
 }
