ff4d8011ecb379223d23e0fd99cd06a3de9783b4
anhquang2605 <anhquang2605@gmail.com>
2025-08-01 15:31:42 -0700

    added api interaction

---
+++ libs/api-interactions.ts
@@ -0,0 +1,80 @@
+const { URL } = process.env;
+const DEFAULT_API_URL = "";
+export function fetchFromGetAPI (path: string, options: any){
+    const baseURL = URL || DEFAULT_API_URL;
+    const url = `${baseURL}/api/${path}`;
+    const params = new URLSearchParams(options).toString();
+    return fetch(`${url}?${params}`, {}).then(res => {
+        if(res.ok){
+            return res.json();
+        }else{
+            return Promise.reject(res);
+        }
+    });
+}
+
+export function updateToPutAPI(path: string, body: any){
+    const baseURL = URL || DEFAULT_API_URL;
+    const url = `${baseURL}/api/${path}`;
+    return fetch(url, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }).then(res => {
+        if(res.ok){
+            return res.json();
+        }else{
+            return Promise.reject(res);
+        }
+    });
+}
+
+export function insertToPostAPI(path: string, body: any){
+    const baseURL = URL || DEFAULT_API_URL;
+    const url = `${baseURL}/api/${path}`;
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }).then(res => {
+        if(res.ok){
+            return res.json();
+        }else{
+            return Promise.reject(res);
+        }
+    });
+}
+export function uploadFileToPostAPI(path: string, file: any){
+    const baseURL = URL || DEFAULT_API_URL;
+    const url = `${baseURL}/api/${path}`;
+    const formData = new FormData();
+    formData.append('file', file);
+    return fetch(url, {
+        method: "POST",
+        body: formData
+    }).then(res => {
+        if(res.ok){
+            return res.json();
+        }else{
+            return Promise.reject(res);
+        }
+    });
+}
+export function deleteFromDeleteAPI(path: string, params: any){
+    const baseURL = URL || DEFAULT_API_URL;
+    const url = `${baseURL}/api/${path}`;
+    const paramsString = new URLSearchParams(params).toString();
+    return fetch(`${url}?${paramsString}`, {
+        method: "DELETE"
+    }).then(res => {
+        if(res.ok){
+            return res.json();
+        }else{
+            return Promise.reject(res);
+        }
+    });
+}
