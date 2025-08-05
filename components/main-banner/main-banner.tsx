c986124affdb54ba749e834a5099e06d2cd668c7
Anh Quang Chu <anhquang2605@gmail.com>
2025-08-05 12:20:12 -0700

    replaced image with background image for banner

--- components/main-banner/main-banner.tsx
+++ components/main-banner/main-banner.tsx
@@ -23,14 +23,6 @@ const ACTIONS = [
 const MainBanner: React.FC<MainBannerProps> = ({}) => {
     return (
         <div className={styles['main-banner']}>
-            <Image
-                src="/asset/images/banner.png"
-                alt="Main Banner"
-                width={1920}
-                height={1080}
-                layout="responsive"
-                className="cover h-1/2"
-            />   
             <InvitationBtns 
                 actions={ACTIONS}
             />
