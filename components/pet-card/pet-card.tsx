c0a960b353e794794db09dbf1a2a07d1d58d95b3
Anh Quang Chu <anhquang2605@gmail.com>
2025-08-08 12:58:32 -0700

    added two digit round

--- components/pet-card/pet-card.tsx
+++ components/pet-card/pet-card.tsx
@@ -22,7 +22,7 @@ const PetCard: React.FC<PetCardProps> = ({
             <Image className={style['pet-image']} src={imageUrl} alt={name} width={300} height={400} /> 
             <section className={style['pet-details']}>
                 <h5 className={style['pet-name']}>{name}</h5>
-                <span className={`${style['pet-price']} ${discount !== 1 ? style['discount'] : ''}`}>{price * discount}</span>
+                <span className={`${style['pet-price']} ${discount !== 1 ? style['discount'] : ''}`}>{(price * discount).toFixed(2)}</span>
                 {discount !== 1 && <span className={style['original-price']}>{price}</span>}
                 <ActionButton type="add" title="Buy Now" />
             </section>
