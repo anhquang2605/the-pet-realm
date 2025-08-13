e4b319dc439de81e7a4d2f07770a1110916e13f7
Anh Quang Chu <anhquang2605@gmail.com>
2025-08-13 10:44:07 -0700

    handle working on navigation items

--- components/navigations/navigation-bar/navigation-bar.tsx
+++ components/navigations/navigation-bar/navigation-bar.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import style from './navigation-bar.module.css';
+import { useRouter } from 'next/router';
 
 interface NavigationBarProps {
 
 }
+interface NavigationItem {
+    title: string;
+    href: string;
+}
+const NAVIGATION_ITEMS = [
+    { title: 'Home', href: '/' },
+    { title: 'Browse Shop', href: '/blog' },
+    { title: 'About', href: '/about' },
+    { title: 'Contact', href: '/contact' },
+
+]
 
 const NavigationBar: React.FC<NavigationBarProps> = ({}) => {
+    const router = useRouter();
+    const handleOnClick = (href: string) => {
+        router.push(href);
+    }
     return (
-        <div className={style['navigation-bar']}>
-            NavigationBar
-        </div>
+        <nav className={style['navigation-bar']}>
+            <ul className={style['navigation-list']}>
+                {NAVIGATION_ITEMS.map((item: NavigationItem, index: number) => (
+                    <li key={index} className={style['navigation-item']} onClick={() => handleOnClick(item.href)}>
+                        {item.title}
+                    </li>
+                ))}
+            </ul>
+        </nav>
     );
 };
 
