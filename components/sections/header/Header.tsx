73a7422f7b1fd321e61d337a3121a65adec84c66
Anh Quang Chu <anhquang2605@gmail.com>
2025-08-14 12:24:15 -0700

    working on mobile version of menu

--- components/sections/header/Header.tsx
+++ components/sections/header/Header.tsx
@@ -2,17 +2,33 @@ import React from 'react';
 import style from './header.module.css';
 import NavigationBar from '../../navigations/navigation-bar/navigation-bar';
 import HomeIcon from './home-icon/home-icon';
-
+import { IoMenu } from "react-icons/io5";
+import { IoCloseCircle } from "react-icons/io5";
 interface HeaderProps {
 
 }
 
 const Header: React.FC<HeaderProps> = ({}) => {
+    const handleMobileMenuClick = () => {
+        const $header = document.querySelector(`.${style['header']}`);
+        if ($header) {
+            $header.classList.toggle(style['opened']);
+        }
+    }
     return (
-        <header className={style['header'] + ' main-bg'}>
-            <HomeIcon />
-            <NavigationBar />
-        </header>
+        <>
+            <button className={style['mobile-menu-button']} onClick={handleMobileMenuClick}>
+                <IoMenu size={30} />
+            </button>
+            <header className={style['header'] + ' main-bg'}>
+                <button className={style['exit-menu-button']} onClick={handleMobileMenuClick}>
+                    <IoCloseCircle size={48} />
+                </button>
+                <HomeIcon />
+                <NavigationBar />
+            </header>
+        </>
+
     );
 };
 
