47cbe71ac6677bc69558317293aafab97006646c
anhquang2605 <anhquang2605@gmail.com>
2025-08-19 18:42:20 -0700

    added another data type for order

--- types/order.ts
+++ types/order.ts
@@ -2,12 +2,24 @@ import { ObjectId } from "mongodb";
 
 export interface Order{
     name: string;
-    id: string | ObjectId;
+    id: string;
     price: number;
     dateCreated: Date;
     status: 'pending' | 'completed' | 'cancelled' | 'fresh'; // e.g., 'pending', 'completed', 'cancelled'
     description: string; // Optional field for additional details
     dateUpdated: Date; // Optional field for tracking updates
+    orderImageUrls: string[]; // Array of image URLs associated with the order
+    discount: number; // Optional field for discount percentage
+    isFeatured: boolean;
+}
+export interface RawOrder{
+     name: string;
+    id: string;
+    price: number;
+    dateCreated: string; // ISO string format
+    status: 'pending' | 'completed' | 'cancelled' | 'fresh'; // e.g., 'pending', 'completed', 'cancelled'
+    description: string; // Optional field for additional details
+    dateUpdated: string; // Optional field for tracking updates
     orderImageUrls: string[]; // Array of image URLs associated with the order
     discount: number; // Optional field for discount percentage
     isFeatured: boolean;
