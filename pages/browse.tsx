7e27536fcf3df66c84e31d9c822756b0a64f0f8c
anhquang2605 <anhquang2605@gmail.com>
2025-08-19 18:13:46 -0700

    working on object of orders

--- pages/browse.tsx
+++ pages/browse.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import style from './page-styles/browse.module.css';
-
+import { browseOrderItems } from '../local_data/mock-order-data';
+import { Order } from '../types/order';
 interface BrowseProps {
-
+    orders: Order[]
+}
+const fetchBrowseData = async () => {
+    // Simulate fetching data for the browse page
+    const data =  browseOrderItems; // This would be replaced with an actual API call
+    return data;
 }
 export async function getStaticProps() {
-    const props: BrowseProps = {};
+    const props: BrowseProps = {
+        orders: [] // Initialize with an empty array or fetch actual data
+    };
+    // Fetch data for the browse page
+    const browseData = await fetchBrowseData()
+    if (browseData) {
+        props.orders = browseData;
+    }
     return { props,
     };
 }
 const Browse: React.FC<BrowseProps> = ({}) => {
     return (
-        <div className={style['']}>
+        <div className={style['browse']}>
             Browse
         </div>
     );
