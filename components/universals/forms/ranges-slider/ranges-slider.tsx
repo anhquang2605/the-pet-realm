42c86a46eb95f496e4e67af6ea461ee9f78cfb6e
anhquang2605 <anhquang2605@gmail.com>
2025-08-27 19:10:53 -0700

    revamping the range slider

--- components/universals/forms/ranges-slider/ranges-slider.tsx
+++ components/universals/forms/ranges-slider/ranges-slider.tsx
@@ -35,50 +35,8 @@ const RangeSlider:React.FC<RangeSliderProps> = ({ min = 0, max = 100, step = 1, 
             right: `${100 - ((range[1] - min) / (max - min)) * 100}%`
           }}
         ></div>
+    
 
-        {/* Lower knob */}
-        <input
-          type="range"
-          min={min}
-          max={max}
-          step={step}
-          value={range[0]}
-          onChange={(e) => handleChange(0, e.target.value)}
-          className="
-            absolute w-full top-0 h-2 bg-transparent appearance-none pointer-events-auto
-            [&::-webkit-slider-thumb]:appearance-none
-            [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4
-            [&::-webkit-slider-thumb]:rounded-full
-            [&::-webkit-slider-thumb]:bg-blue-600
-            [&::-webkit-slider-thumb]:cursor-pointer
-            [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-white
-            [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:w-4
-            [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-blue-600
-            [&::-moz-range-thumb]:cursor-pointer [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-white
-          "
-        />
-
-        {/* Upper knob */}
-        <input
-          type="range"
-          min={min}
-          max={max}
-          step={step}
-          value={range[1]}
-          onChange={(e) => handleChange(1, e.target.value)}
-          className="
-            absolute w-full top-0 h-2 bg-transparent appearance-none pointer-events-auto
-            [&::-webkit-slider-thumb]:appearance-none
-            [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4
-            [&::-webkit-slider-thumb]:rounded-full
-            [&::-webkit-slider-thumb]:bg-blue-600
-            [&::-webkit-slider-thumb]:cursor-pointer
-            [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-white
-            [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:w-4
-            [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-blue-600
-            [&::-moz-range-thumb]:cursor-pointer [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-white
-          "
-        />
       </div>
 
       {/* Value display */}
