f96d6baba53983017e0bc9c966949d5cfd508c76
anhquang2605 <anhquang2605@gmail.com>
2025-09-03 10:44:53 -0700

    styling the browse page

--- components/sections/browse-components/order-fitler/order-filter.tsx
+++ components/sections/browse-components/order-fitler/order-filter.tsx
@@ -56,7 +56,7 @@ const OrderFilter: React.FC<OrderFilterProps> = ({priceRange, setFilter}) => {
 
     return (
         <section id="order-filter" className={style['order-filter']}>
-            <h3 className={style['order-filter__title']}><BsFilter /> <span className="order-filter__text">Filter</span></h3>
+            <h3 className={style['order-filter__title']}><BsFilter /> <span className={style["order-filter__text"]}>Filter</span></h3>
             {/* Filter UI elements go here */}
             <fieldset className={`${style['price-range']}`}>
                 <legend>Price Range</legend>
