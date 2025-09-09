e613a70e7e6c5f14b31c7c2fb9616d31da2fe480
anhquang2605 <anhquang2605@gmail.com>
2025-09-08 19:18:49 -0700

    working on sorter

--- components/sections/browse-components/order-sorter/order-sorter.tsx
+++ components/sections/browse-components/order-sorter/order-sorter.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import style from './order-sorter.module.css';
+import DropDownList from '../../../universals/drop-down-list/drop-down-list';
 
 interface OrderSorterProps {
 
 }
-
+const sortingOptions = [
+    "Price",
+    "Name",
+    "Date Added",
+]
 const OrderSorter: React.FC<OrderSorterProps> = ({}) => {
     return (
         <div className={style['order-sorter']}>
-            
+            {
+                <DropDownList 
+                    items={sortingOptions} 
+                    actions={sortingOptions.map((option) => () => {})} 
+                    placeholder="Sort By"
+                />
+            }
         </div>
     );
 };
