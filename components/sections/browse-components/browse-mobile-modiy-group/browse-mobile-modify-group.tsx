bbebd573de2270de7062057856695121238c46c8
anhquang2605 <anhquang2605@gmail.com>
2025-09-18 15:34:59 -0700

    added mobile modify group to group filter and sorter components in mobile mode of the browse page

---
+++ components/sections/browse-components/browse-mobile-modiy-group/browse-mobile-modify-group.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './browse-mobile-modify-group.module.css';
+
+interface BrowseMobileModifyGroupProps {
+
+}
+
+const BrowseMobileModifyGroup: React.FC<BrowseMobileModifyGroupProps> = ({}) => {
+    return (
+        <div className={style['browse-mobile-modify-group']}>
+            BrowseMobileModifyGroup
+        </div>
+    );
+};
+
+export default BrowseMobileModifyGroup;
