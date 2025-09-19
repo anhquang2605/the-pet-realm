a4e6cede4c3c756dc9d187dd37017cfbcb5d598f
anhquang2605 <anhquang2605@gmail.com>
2025-09-19 09:02:12 -0700

    trying to set up context provider

--- components/sections/browse-components/use-order-fitler/order-filter.tsx
+++ components/sections/browse-components/use-order-fitler/order-filter.tsx
@@ -25,7 +25,23 @@ interface useOrderFilterContext {
 }
 
 interface FilterContextProviderProps {
-    
+    setFilter: React.Dispatch<React.SetStateAction<OrderFilterI>>;
+    priceRange: [number, number];
+    children: React.ReactNode;
+}
+
+const FilterContext = React.createContext<useOrderFilterContext>({} as useOrderFilterContext);
+
+const FilterContextProvider: React.FC<FilterContextProviderProps> = ({children, setFilter, priceRange}) => {
+    const revealMobileFilter = () => {
+        const filterSection = document.getElementsByClassName(style['order-filter'])[0];
+        filterSection.classList.toggle(style['order-filter--active']);
+    }
+    return (
+        <FilterContext.Provider value={{revealMobileFilter}}>
+            {children}
+        </FilterContext.Provider>
+    );
 }
 
 const OrderFilter: React.FC<OrderFilterProps> = ({priceRange, setFilter}) => {
