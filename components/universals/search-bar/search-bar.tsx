a2d0aa505b8f8331b8229b9c6a3e784bcc10e136
anhquang2605 <anhquang2605@gmail.com>
2025-09-29 17:27:41 -0700

    added toggle active class

--- components/universals/search-bar/search-bar.tsx
+++ components/universals/search-bar/search-bar.tsx
@@ -9,14 +9,14 @@ const SearchBar: React.FC<SearchBarProps> = ({}) => {
     const toggleSearchBar = () => {
         const searchBarPlaceholder = document.getElementsByClassName(style['search-bar-placeholder'])[0];
         const searchBarInput = document.getElementsByClassName(style['search-bar-input'])[0];
-        searchBarPlaceholder.classList.toggle(style['search-bar-placeholder--active']);
-        searchBarInput.classList.toggle(style['search-bar-input--active']);
+        searchBarPlaceholder.classList.toggle(style['search-bar--active']);
+        searchBarInput.classList.toggle(style['search-bar--active']);
     }
     return (
         <div className={style['search-bar']}>
            <span onClick={
                 toggleSearchBar
-            } className={style['search-bar-placeholder']} >Find your pet!</span>
+            } className={style['search-bar-placeholder'] + " " + style['search-bar--active'] } >Find your pet!</span>
            <input className={style['search-bar-input']} type="text" />
            <IoSearch />
         </div>
