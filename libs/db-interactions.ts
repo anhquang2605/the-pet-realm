2d511c16a3f60b89650a113632f166289fd9d65d
anhquang2605 <anhquang2605@gmail.com>
2025-10-03 14:23:06 -0700

    removed the content of db interactions

--- libs/db-interactions.ts
+++ libs/db-interactions.ts
@@ -1,39 +1,5 @@
-import { pick } from "lodash";
-import { User, UserInClient } from "../types/User";
+import { Order } from "../types/order";
 
-export async function getUserDataByUserName(username: string){
-    try{
-       const path = '/api/users?username=' + username;
-       const options = {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json'
-              }
-       }
-       const res = await fetch(path, options);
-       if(res.status == 200){
-           const data = await res.json();
-           if(data.success){
-                return data.data;
-           }
-       }
-       return null;
-    }catch(e){
-        console.log(e);
-        return null;
-    }
-}
-export const pickUserInClient = (user: User) => {
-    const pickedUser = pick(user, [
-        'username',
-        'location',
-        'email',
-        'featuredWeather',
-        'firstName',
-        'lastName',
-        'profilePicturePath',
-        'dateJoined',
-        'backgroundPicturePath'
-    ])
-    return pickedUser as UserInClient;
+export async function getCollectionFromDB(db: string){
+    const collection =  
 }
