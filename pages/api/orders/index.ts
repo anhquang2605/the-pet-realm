303d6e4df90ad4fe07c9c90302e2f00feed5865c
anhquang2605 <anhquang2605@gmail.com>
2025-10-06 17:24:01 -0700

    tried to insert one and many orders

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -38,7 +38,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       // Delete a order
       res.status(200).json({ message: `Order ${id} deleted` });
       break;
-
+    case "POST":
+      // Create a new order
+      const newData = req.body;
+      if(!newData) return res.status(400).json({ message: "No data provided" });
+      if(newData && newData.length === 1){
+        const newOrder: Order = newData[0];
+        try{
+          ordersCollection.insertOne(newOrder);
+          return res.status(201).json({ message: "Order created", data: newOrder });
+        } catch(e){
+          return res.status(500).json({ message: "Error creating order", error: e });
+        }
+      }
+      if(newData && newData.length > 1){
+        const newOrders: Order[] = newData;
+        try{
+          ordersCollection.insertMany(newOrders);
+          return res.status(201).json({ message: "Orders created", data: newOrders });
+        } catch(e){
+          return res.status(500).json({ message: "Error creating orders", error: e });
+        }
+      }
+      break;
     default:
       res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
       res.status(405).end(`Method ${req.method} Not Allowed`);
