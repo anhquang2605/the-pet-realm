0ecddfa0f92ec754c50c4a7896126c8d758b04ca
anhquang2605 <anhquang2605@gmail.com>
2025-10-06 17:12:01 -0700

    added order collection db helper

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Order } from "../../../types/order";
+import { getCollectionFromDB } from "../../../libs/db-interactions";
 
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id, name, ids } = req.query;
-
+  const ordersCollection = getCollectionFromDB("orders");
   switch (req.method) {
     case "GET":
       // Get a order
@@ -12,20 +13,21 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
       ];
       if(id && typeof id === "string"){
-        const order = orders.find(order => order.id === id);
+        const order = ordersCollection?.find
         if(!order) return res.status(404).json({ message: "Order not found" });
         return res.status(200).json(order);
       } else if(ids && Array.isArray(ids)){
-        const filteredOrders = orders.filter(order => ids.includes(order.id.toString()));
+       
         return res.status(200).json(filteredOrders);
       } else if (name && typeof name === "string"){
-        const filteredOrders = orders.filter(order => order.name === name);
+        
         return res.status(200).json(filteredOrders);
       }
         break;
     case "PUT":
       // Update a order
       const updateData = req.body;
+
       res.status(200).json({ message: `Order ${id} updated`, data: updateData });
       break;
 
