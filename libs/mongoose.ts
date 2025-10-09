d75576a1280d1825b5fe342c36deed04f9820389
anhquang2605 <anhquang2605@gmail.com>
2025-10-08 17:55:25 -0700

    working on cached

--- libs/mongoose.ts
+++ libs/mongoose.ts
@@ -21,14 +21,17 @@ if (!cached) {
 }
 
 export async function connectDB() {
-  if (!cached) return null;
-  if (cached.conn) return cached.conn;
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-    bufferCommands: false,
-    }).then((mongoose) => mongoose);
+ 
+  if (cached?.conn) return cached.conn;
+  if( cached !== undefined){
+    if (cached.promise) {
+      cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+      }).then((mongoose) => mongoose);
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
   }
-  cached.conn = await cached.promise;
-  console.log('Connected to MongoDB');
-  return cached.conn;
+  
+  
 }
