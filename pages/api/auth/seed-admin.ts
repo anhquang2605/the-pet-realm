d5082694a7e62e1b865a35c6939619ca519ca721
anhquang2605 <anhquang2605@gmail.com>
2025-10-08 17:30:53 -0700

    added try catch

--- pages/api/auth/seed-admin.ts
+++ pages/api/auth/seed-admin.ts
@@ -5,12 +5,23 @@ import Admin from './../../../models/Admin';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await connectDB();
-
-  const existing = await Admin.findOne({ email: 'admin@shop.com' });
-  if (existing) return res.status(200).json({ message: 'Admin already exists' });
+  try {
+     const existing = await Admin.findOne({ email: 'admin@shop.com' });
+      if (existing) return res.status(200).json({ message: 'Admin already exists' });
+    } catch (error) {
+    return res.status(500).json({ message: 'Database error while checking admin', error });
+  }
+ 
+ 
 
   const hashed = await bcrypt.hash('admin123', 10);
-  await Admin.create({ email: 'admin@shop.com', password: hashed, name: 'Shop Owner' });
+  try{
+      await Admin.create({ email: 'admin@shop.com', password: hashed, name: 'Shop Owner' });
+      res.status(201).json({ message: 'Default admin created', email: 'admin@shop.com', password: 'admin123' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Database error, cannot create default admin', error });
+  }
 
-  res.status(201).json({ message: 'Default admin created', email: 'admin@shop.com', password: 'admin123' });
+
+ 
 }
