95a870941a19c5ec019b91f745c6c8c5fc0a9dcb
anhquang2605 <anhquang2605@gmail.com>
2025-10-08 17:39:08 -0700

    working on mongoose

--- pages/api/auth/seed-admin.ts
+++ pages/api/auth/seed-admin.ts
@@ -6,10 +6,11 @@ import Admin from './../../../models/Admin';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await connectDB();
   try {
-     const existing = await Admin.findOne({ email: 'admin@shop.com' });
+     const existing = await Admin.find({ email: 'admin@shop.com' });
       if (existing) return res.status(200).json({ message: 'Admin already exists' });
-    } catch (error) {
-    return res.status(500).json({ message: 'Database error while checking admin', error });
+  } catch (error) {
+      console.log(error);
+   
   }
  
  
