b30a7dfa3b8a36c20980017234c8f285ef4798d3
anhquang2605 <anhquang2605@gmail.com>
2025-10-08 18:03:09 -0700

    removed redudancy

--- libs/mongoose.ts
+++ libs/mongoose.ts
@@ -5,7 +5,10 @@ const MONGODB_URI = process.env.MONGODB_URI as string;
 if (!MONGODB_URI) {
   throw new Error('Please define MONGODB_URI in your .env.local file');
 }
-
+type MongooseCache = {
+  conn : typeof mongoose | null;
+  promise : Promise<typeof mongoose> | null;
+}
 declare global {
   // eslint-disable-next-line no-var
   var mongooseCache: {
@@ -14,24 +17,15 @@ declare global {
   } | undefined;
 }
 
-let cached = global.mongooseCache;
-
-if (!cached) {
-  cached = global.mongooseCache = { conn: null, promise: null };
-}
+let cached: MongooseCache  = global.mongooseCache || { conn: null, promise: null };
 
 export async function connectDB() {
- 
-  if (cached?.conn) return cached.conn;
-  if( cached !== undefined){
-    if (cached.promise) {
-      cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-      }).then((mongoose) => mongoose);
-    }
-    cached.conn = await cached.promise;
-    return cached.conn;
+  if (cached.conn) return cached.conn;
+  if (cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+    bufferCommands: false,
+    }).then((mongoose) => mongoose);
   }
-  
-  
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
