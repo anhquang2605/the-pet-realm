15789ecffd47cd28ca9f0e60d0012843b660245d
anhquang2605 <anhquang2605@gmail.com>
2025-10-13 17:27:08 -0700

    fixed

--- libs/mongoose.ts
+++ libs/mongoose.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
+const DB_NAME = process.env.MONGO_DB as string;
 if (!MONGODB_URI) {
   throw new Error('Please define MONGODB_URI in your .env.local file');
 }
@@ -20,13 +21,12 @@ let cached: MongooseCache  = global.mongooseCache || { conn: null, promise: null
 
 export async function connectDB() {
   if (cached.conn) return cached.conn;
-  if (cached.promise) {
+  if (cached.promise === null) {
     
     const conn = mongoose.connect(MONGODB_URI, {
     bufferCommands: false,
-    
+    dbName: DB_NAME
     }).then((mongoose) => mongoose);
-    console.log(conn);
     cached.promise = conn;
   }
   cached.conn = await cached.promise;
