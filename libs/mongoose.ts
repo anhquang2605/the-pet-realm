3ed92efc0181c891fc32b283af0527cb8903ac64
anhquang2605 <anhquang2605@gmail.com>
2025-10-13 17:14:26 -0700

    capture connection

--- libs/mongoose.ts
+++ libs/mongoose.ts
@@ -21,9 +21,13 @@ let cached: MongooseCache  = global.mongooseCache || { conn: null, promise: null
 export async function connectDB() {
   if (cached.conn) return cached.conn;
   if (cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
+    
+    const conn = mongoose.connect(MONGODB_URI, {
     bufferCommands: false,
+    
     }).then((mongoose) => mongoose);
+    console.log(conn);
+    cached.promise = conn;
   }
   cached.conn = await cached.promise;
   return cached.conn;
