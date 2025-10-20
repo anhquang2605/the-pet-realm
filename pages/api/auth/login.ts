97d05fd9ea22180e838f2f42485fd5f0dbdbdaa2
Anh Quang Chu <anhquang2605@gmail.com>
2025-10-20 13:24:12 -0700

    added jose

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import bcrypt from 'bcryptjs';
 import { connectDB } from './../../../libs/mongoose';
 import Admin from './../../../models/Admin';
-
+import {SignJWT} from 'jose';
 const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -19,6 +19,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const valid = await bcrypt.compare(password, admin.password);
   if (!valid) return res.status(401).json({ message: 'Invalid credentials' });
 
-  const token = jwt.sign({ id: admin._id, role: 'admin' }, JWT_SECRET, { expiresIn: '1h' });
-  res.status(200).json({ message: 'Login successful', token });
+ // const token = jwt.sign({ id: admin._id, role: 'admin' }, JWT_SECRET, { expiresIn: '1h' });
+ const token = await new SignJWT({ email: admin.email, role: 'admin' }).setProtectedHeader({ alg: 'HS256' }).setIssuedAt().setExpirationTime('3h').sign(new TextEncoder().encode(JWT_SECRET)); 
+ res.status(200).json({ message: 'Login successful', token });
 }
