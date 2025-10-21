a594fce87e7589399574b128e13650f17ea7b4ea
Anh Quang Chu <anhquang2605@gmail.com>
2025-10-21 10:11:37 -0700

    added action button

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,9 +2,17 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { jwtVerify } from 'jose';
 import styles from './page-styles/admin.module.css';
+import ActionCard from '../components/sections/admin-components/action-card/action-card';
+import ActionButton from '../components/universals/buttons/action-button/action-button';
 
 const JWT_SECRET = process.env.NEXT_PUBLIC_JWT_SECRET || 'supersecretkey';
 const loginPage = '/authentication/login';
+const ADMIN_ACTIONS = [
+  { title: 'Browse Orders', description: 'View and manage all orders in the system.', link: '/browse/admin' },
+  {title: 'Create new Order', description: 'Create a new order on behalf of a user.', link: '/orders/create' },
+  { title: 'View Mailbox', description: 'View and manage all emails in the system.', link: '/mailbox' },
+
+]
 export default function AdminPage() {
   const router = useRouter();
   const [isAuthorized, setIsAuthorized] = useState(false);
@@ -65,7 +73,13 @@ export default function AdminPage() {
     <div className={"h-screen flex flex-col items-center justify-center bg-green-100" + styles['admin']}>
       <h1 className="text-3xl font-bold text-green-700">Welcome, Admin!</h1>
       <div className={styles['admin__dashboard']}>
-        
+        {
+          ADMIN_ACTIONS.map((action, index) => (
+            <ActionCard key={index} title={action.title} description={action.description}>
+              <ActionButton type="main" title={action.title} onClick={() => router.push(action.link)} />     
+            </ActionCard>
+          ))
+        }
       </div>
     </div>
   );
