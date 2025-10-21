1bbbb65607464328ef4b346a89bc29fab1a5ac5e
Anh Quang Chu <anhquang2605@gmail.com>
2025-10-21 09:41:08 -0700

    setting up form

--- components/sections/admin-components/order-form/order-form.tsx
+++ components/sections/admin-components/order-form/order-form.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './order-form.module.css';
 import { Order } from '../../../../types/order';
 interface OrderFormProps {
-
+  onSubmit: (orderData: Omit<Order, 'id' | 'dateCreated' | 'dateUpdated'>) => void;
+  isLoading?: boolean;
 }
-
+interface OrderFormData{
+    name: string;
+    price: number;
+    description: string;
+    discount: number;
+    isFeatured: boolean;
+}
 const OrderForm: React.FC<OrderFormProps> = ({}) => {
+    const [formData, setFormData] = useState<OrderFormData>({
+        name: '',
+        price: 0,
+        description: '',
+        discount: 0,
+        isFeatured: true,
+    });
+    const [uploadedImages, setUploadedImages] = useState<string[]>([]);
+    const [isUploading, setIsUploading] = useState(false);
     return (
         <div className={style['order-form']}>
             OrderForm
