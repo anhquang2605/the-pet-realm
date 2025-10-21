a5afc605c1b9f9781d7ff1cc8fa2801b3d9f37ea
Anh Quang Chu <anhquang2605@gmail.com>
2025-10-21 10:07:36 -0700

    defined the action card

--- components/sections/admin-components/action-card/action-card.tsx
+++ components/sections/admin-components/action-card/action-card.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import style from './action-card.module.css';
+import { title } from 'process';
 
 interface ActionCardProps {
-
+    title?: string;
+    description?: string;
+    children?: React.ReactNode; 
 }
 
-const ActionCard: React.FC<ActionCardProps> = ({}) => {
+const ActionCard: React.FC<ActionCardProps> = ({
+    title = "Action Card Title",
+    description = "This is a description for the action card.",
+    children
+}) => {
     return (
         <div className={style['action-card']}>
-            ActionCard
+            <h5>{title}</h5>
+            <p>{description}</p>
+            <div>
+                {children}
+            </div>
         </div>
     );
 };
