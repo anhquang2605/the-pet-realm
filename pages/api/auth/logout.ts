a69dc01d8e9d336e8dfcd0342bd3716359f67666
Anh Quang Chu <anhquang2605@gmail.com>
2025-10-24 11:15:17 -0700

    working on logout

---
+++ pages/api/auth/logout.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcryptjs';
+import { connectDB } from './../../../libs/mongoose';
+import Admin from './../../../models/Admin';
+import {SignJWT} from 'jose';
+const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'DELETE') return res.status(405).end();
+
+  await connectDB();
+
+  const { email, password } = req.body;
+  if (!email || !password) return res.status(400).json({ message: 'Missing fields' });
+
+  const admin = await Admin.findOne({ email });
+  if (!admin) return res.status(401).json({ message: 'Invalid credentials' });
+
+  const valid = await bcrypt.compare(password, admin.password);
+  if (!valid) return res.status(401).json({ message: 'Invalid credentials' });
+
+ // const token = jwt.sign({ id: admin._id, role: 'admin' }, JWT_SECRET, { expiresIn: '1h' });
+ const token = await new SignJWT({ email: admin.email, role: 'admin' }).setProtectedHeader({ alg: 'HS256' }).setIssuedAt().setExpirationTime('3h').sign(new TextEncoder().encode(JWT_SECRET)); 
+ res.status(200).json({ message: 'Login successful', token });
+}
