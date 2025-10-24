8e2985944895ef8a2aebd0d0b2a4556a5be3634a
Anh Quang Chu <anhquang2605@gmail.com>
2025-10-24 11:37:25 -0700

    added more style

--- pages/authentication/logout.tsx
+++ pages/authentication/logout.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import style from './../page-styles/logout.module.css';
+import Image from 'next/image';
 
 interface LogoutPageProps {
 
 }
 
 const LogoutPage: React.FC<LogoutPageProps> = ({}) => {
+    const [message, setMessage] = React.useState('Logging out...');
     return (
         <section className={style['logout-page']}>
-            LogoutPage
+            <Image src="/asset/images/racoon.webp" alt="Racoon pet image from grow a garden game" width={250} height={250} />
+            <h2>
+                {message}
+            </h2>
         </section>
     );
 };
