9e1ba259ed15a093d5a4078d061b3c53f873a44d
Anh Quang Chu <anhquang2605@gmail.com>
2025-10-24 11:43:25 -0700

    added logout api talk

--- pages/authentication/logout.tsx
+++ pages/authentication/logout.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './../page-styles/logout.module.css';
 import Image from 'next/image';
+import ActionButton from '../../components/universals/buttons/action-button/action-button';
 
 interface LogoutPageProps {
-
+    
 }
 
 const LogoutPage: React.FC<LogoutPageProps> = ({}) => {
     const [message, setMessage] = React.useState('Logging out...');
+    const performLogout = async () => {
+        const token = localStorage.getItem('admin_token');
+        if (!token) {
+            setMessage('No active session found.');
+            return;
+        }
+        const res = await fetch('/api/auth/logout', {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        });
+        const data = await res.json();
+        if (res.ok) {
+            localStorage.removeItem('admin_token');
+            setMessage('Logout successful. Redirecting to home page...');
+            setTimeout(() => {
+                window.location.href = '/';
+            }, 3000);
+        } else {
+            setMessage(data.message || 'Logout failed. Please try again.');
+        }
+    };
+    
+    useEffect(() => {
+        
+    },[])
     return (
         <section className={style['logout-page']}>
-            <Image src="/asset/images/racoon.webp" alt="Racoon pet image from grow a garden game" width={250} height={250} />
-            <h2>
-                {message}
-            </h2>
+            <div className={style['logout-page__content']}>
+                 <Image src="/asset/images/racoon.webp" alt="Racoon pet image from grow a garden game" width={150} height={150} />
+                <h2 className="text-white text-2xl mt-1">
+                    {message}
+                    <ActionButton type="main" title="Back" onClick={} />
+                </h2>
+            </div>
         </section>
     );
 };
