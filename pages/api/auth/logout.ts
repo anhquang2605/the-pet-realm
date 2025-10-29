338020e05cd19c4e94a980558ca01895c8704581
anhquang2605 <anhquang2605@gmail.com>
2025-10-28 18:35:26 -0700

    changed varible name to import from env file

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import { connectDB } from './../../../libs/mongoose';
 import Admin from './../../../models/Admin';
 import {SignJWT} from 'jose';
-const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey';
+const JWT_SECRET = process.env.NEXT_PUBLIC_JWT_SECRET || 'supersecretkey';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'DELETE') return res.status(405).end();
