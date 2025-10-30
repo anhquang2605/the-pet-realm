6d4df8182924a0396667435c867ad8dc28b8c5eb
anhquang2605 <anhquang2605@gmail.com>
2025-10-29 19:02:46 -0700

    added base class for form

--- components/sections/admin-components/order-form/order-form.tsx
+++ components/sections/admin-components/order-form/order-form.tsx
@@ -98,7 +98,7 @@ const OrderForm: React.FC<OrderFormProps> = ({
     };
 
     return (
-        <div className={style['order-form'] + ' ' + "max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md"}>
+        <div className={style['order-form'] + ' ' + "max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md flex flex-col"}>
             <h2 className="text-2xl font-bold mb-6 text-gray-800">Create New Order</h2>
             {/* Form */}
             <form onSubmit={handleSubmit} className="space-y-6">
