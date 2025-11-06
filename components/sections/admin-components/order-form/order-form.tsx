039df951606e12f0c551327a0e1372cf7c04cc95
anhquang2605 <anhquang2605@gmail.com>
2025-11-06 15:04:55 -0800

    removed flex col

--- components/sections/admin-components/order-form/order-form.tsx
+++ components/sections/admin-components/order-form/order-form.tsx
@@ -101,7 +101,7 @@ const OrderForm: React.FC<OrderFormProps> = ({
         <div className={style['order-form'] + ' ' + "mx-auto rounded-lg shadow-md flex flex-col"}>
             <h2 className="text-2xl font-bold mb-1 text-slate-200">Create New Order</h2>
             {/* Form */}
-            <form onSubmit={handleSubmit} className=" flex flex-col">
+            <form onSubmit={handleSubmit} className=" flex">
                 {/* Name Field */}
                 <div className={style['full']}>
                 <label htmlFor="name" className="block text-sm font-medium  ">
