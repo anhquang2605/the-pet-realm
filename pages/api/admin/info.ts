842b449ce53ad5e5d14c6c11a11f8be47c3c691d
anhquang2605 <anhquang2605@gmail.com>
2025-12-12 17:54:28 -0800

    change method to get

--- pages/api/admin/info.ts
+++ pages/api/admin/info.ts
@@ -4,7 +4,9 @@ import { getCollectionFromDB } from "../../../libs/db-interactions";
 export default async function handler(req: any, res: any) {
     const adminCollection = await getCollectionFromDB("admins");
     if(!adminCollection) return res.status(500).json({ message: "Database connection error" });
-    const admin = await adminCollection.findOne({ email: req.body.email });
+    if(req.method !== "GET") return res.status(405).json({ message: "Method not allowed" });
+    const admin = await adminCollection.findOne({ email: req.query.email });
+    
     if(!admin) return res.status(404).json({ message: "Admin not found" });
     res.status(200).json(admin);
 }
