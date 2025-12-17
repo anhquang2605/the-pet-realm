3dfb26faaa00ab34aff8625d09373db799012ff0
Anh Quang Chu <anhquang2605@gmail.com>
2025-12-16 17:37:31 -0800

    added title string for alternating the button

--- components/universals/reveal-box/reveal-box.tsx
+++ components/universals/reveal-box/reveal-box.tsx
@@ -5,14 +5,21 @@ interface RevealBoxProps {
     children: React.ReactNode;
     hasCustomeRevealButton?: boolean;
     customRevealSetter?: (isRevealed: boolean) => void;
+    buttonStateTitles:[string, string]; // [show, hide]
 }
 
 const RevealBox: React.FC<RevealBoxProps> = ({children , hasCustomeRevealButton = false}) => {
     const [isRevealed, setIsRevealed] = React.useState(false);
-    
+
     return (
         <>
-
+        {
+            hasCustomeRevealButton ?
+            <button className={style['reveal-button']} onClick={() => setIsRevealed(!isRevealed)}>
+                {isRevealed ? "Hide" : "Reveal"}
+            </button>
+            : ''
+        }   
             <div className={style['reveal-box'] + " " + (isRevealed ? style['active'] : '')}>
                 {
                     children
