b828f50f73c9ca421a92821cf57943517b2ef510
Anh Quang Chu <anhquang2605@gmail.com>
2025-12-16 17:32:57 -0800

    added has custome reveal button check

--- components/universals/reveal-box/reveal-box.tsx
+++ components/universals/reveal-box/reveal-box.tsx
@@ -3,14 +3,20 @@ import style from './reveal-box.module.css';
 
 interface RevealBoxProps {
     children: React.ReactNode;
+    hasCustomeRevealButton?: boolean;
 }
 
-const RevealBox: React.FC<RevealBoxProps> = ({}) => {
+const RevealBox: React.FC<RevealBoxProps> = ({children , hasCustomeRevealButton = false}) => {
     const [isRevealed, setIsRevealed] = React.useState(false);
     return (
-        <div className={style['reveal-box']}>
-            RevealBox
-        </div>
+        <>
+
+            <div className={style['reveal-box']}>
+                {
+                    children
+                }
+            </div>
+        </>
     );
 };
 
