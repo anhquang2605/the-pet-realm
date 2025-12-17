853421aa308dd107e67f10296826af7915dc7166
Anh Quang Chu <anhquang2605@gmail.com>
2025-12-16 17:35:32 -0800

    reveal box figure

--- components/universals/reveal-box/reveal-box.tsx
+++ components/universals/reveal-box/reveal-box.tsx
@@ -4,10 +4,12 @@ import style from './reveal-box.module.css';
 interface RevealBoxProps {
     children: React.ReactNode;
     hasCustomeRevealButton?: boolean;
+    customRevealSetter?: (isRevealed: boolean) => void;
 }
 
 const RevealBox: React.FC<RevealBoxProps> = ({children , hasCustomeRevealButton = false}) => {
     const [isRevealed, setIsRevealed] = React.useState(false);
+    
     return (
         <>
 
