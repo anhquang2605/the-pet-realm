18e7001e2665fd8a6c4e66d65cbd7e13225b7af5
Anh Quang Chu <anhquang2605@gmail.com>
2025-12-16 17:30:58 -0800

    allowed children the reveal box

--- components/universals/reveal-box/reveal-box.tsx
+++ components/universals/reveal-box/reveal-box.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import style from './reveal-box.module.css';
 
 interface RevealBoxProps {
-
+    children: React.ReactNode;
 }
 
 const RevealBox: React.FC<RevealBoxProps> = ({}) => {
+    const [isRevealed, setIsRevealed] = React.useState(false);
     return (
         <div className={style['reveal-box']}>
             RevealBox
