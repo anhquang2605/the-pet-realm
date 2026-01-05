565d08ef1d5c19d9c2a6b8e505fed5e7aec9bed9
Anh Quang Chu <anhquang2605@gmail.com>
2026-01-05 11:00:04 -0800

    trying to add message

--- components/openai/chatbot/chatbot.tsx
+++ components/openai/chatbot/chatbot.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { use } from 'react';
 import style from './chatbot.module.css';
-
+import { insertToPostAPI } from '../../../libs/api-interactions';
+import { useState, useEffect } from 'react';
 interface ChatbotProps {
 
 }
 
 const Chatbot: React.FC<ChatbotProps> = ({}) => {
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        
+    },[])
     return (
         <div className={style['chatbot']}>
             Chatbot
         </div>
     );
 };
+const sendMessageToOpenAI = async (message: string) => {
+    const path = '/chat';
+    const reponse = insertToPostAPI(path, {message});
+    return reponse;
+}
 
 export default Chatbot;
