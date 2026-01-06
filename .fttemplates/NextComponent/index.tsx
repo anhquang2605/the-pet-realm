01cc39ffdad8c5ca3bc80b21d8049814d2135d29
Anh Quang Chu <anhquang2605@gmail.com>
2026-01-06 12:54:22 -0800

    working on file template

---
+++ .fttemplates/NextComponent/index.tsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import style from './__name__.module.css';
+
+interface __pascalCase__Props {
+
+}
+
+const __pascalCase__: React.FC<__pascalCase__Props> = ({}) => {
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <div className={style['__name__']}>
+            __pascalCase__Icon
+        </div>
+    );
+};
+
+export default __pascalCase__;
