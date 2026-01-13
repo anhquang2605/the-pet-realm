c3b08879a130f06ce5e1e025ffcab58c2f860954
Anh Quang Chu <anhquang2605@gmail.com>
2026-01-12 18:59:48 -0800

    removed any

--- pages/api/admin/info.ts
+++ pages/api/admin/info.ts
@@ -1,7 +1,8 @@
 //get admin info from mongo db database and return it to the client
+import { NextApiRequest, NextApiResponse } from "next";
 import { getCollectionFromDB } from "../../../libs/db-interactions";
 
-export default async function handler(req: any, res: any) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const adminCollection = await getCollectionFromDB("admins");
     if(!adminCollection) return res.status(500).json({ message: "Database connection error" });
     if(req.method !== "GET") return res.status(405).json({ message: "Method not allowed" });
