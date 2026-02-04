f1c80dfce20753a277a59c232bf8207cf404f6ba
Anh Quang Chu <anhquang2605@gmail.com>
2026-02-03 18:49:40 -0800

    added helper

---
+++ libs/helpers.ts
@@ -0,0 +1,7 @@
+export const binarySearch = <T> (arr: T[], target: T) => {
+    let lower = 0;
+    let upper = arr.length - 1;
+    while (lower <= upper){
+        const mid = Math.floor((lower + upper) / 2);
+    }
+}
