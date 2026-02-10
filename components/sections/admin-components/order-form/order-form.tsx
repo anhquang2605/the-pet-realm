cdbe119dd03a092c23228b3ffff1c98a573180d2
Anh Quang Chu <anhquang2605@gmail.com>
2026-02-09 19:01:56 -0800

    changed

--- components/sections/admin-components/order-form/order-form.tsx
+++ components/sections/admin-components/order-form/order-form.tsx
@@ -256,7 +256,7 @@ const OrderForm: React.FC<OrderFormProps> = ({
                 
 
                 {/* Status Display (Read-only) */}
-                <div className={style['full'] + " " + style['input-wannabe'] + " bg-gray-50 rounded-md py-2!important bg-gray-600 flex justify-center"}>
+                <div className={style['full'] + " " + style['input-wannabe'] + " bg-gray-50 rounded-md py-2!important bg-gray-600 flex flex-col justify-center"}>
                     <p className="text-sm text-gray-300 text-center">
                         Status: <span className="font-bold text-green-600">Fresh</span>
                     </p>
