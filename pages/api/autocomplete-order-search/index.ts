8cbe4247b4a2f1e7066d127a7714c8e6a6127cbc
anhquang2605 <anhquang2605@gmail.com>
2026-04-22 16:30:39 -0700

    working on getting the oders collection

--- pages/api/autocomplete-order-search/index.ts
+++ pages/api/autocomplete-order-search/index.ts
@@ -4,5 +4,8 @@ import { getCollectionFromDB } from "../../../libs/db-interactions";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    
+    const { query } = req.query;
+    const ordersCollection = await getCollectionFromDB("orders");
+    if(!ordersCollection) return res.status(500).json({ message: "Database connection error" });
+
 }
