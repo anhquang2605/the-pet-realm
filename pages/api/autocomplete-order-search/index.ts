e9573bd4a44bbf2a05e0ae7a71095ea39b456b20
anhquang2605 <anhquang2605@gmail.com>
2026-04-22 16:32:35 -0700

    agrregate added

--- pages/api/autocomplete-order-search/index.ts
+++ pages/api/autocomplete-order-search/index.ts
@@ -7,5 +7,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { query } = req.query;
     const ordersCollection = await getCollectionFromDB("orders");
     if(!ordersCollection) return res.status(500).json({ message: "Database connection error" });
-
+    ordersCollection.aggregate([
+        {
+            $search: {
+                index: "the-pet-realm-orders-search",
+                autocomplete: {
+                    query: query,
+                    path: "name",
+                    fuzzy: {
+                        maxEdits: 2,
+                        prefixLength: 3
+                    }
+                }
+            }
+        }
+    ])
 }
