06b3f3826809be78995edf4b71a744d481ad6937
anhquang2605 <anhquang2605@gmail.com>
2026-04-22 16:25:08 -0700

    added based for search

---
+++ pages/api/autocomplete-order-search/index.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Order } from "../../../types/order";
+import { getCollectionFromDB } from "../../../libs/db-interactions";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    
+}
