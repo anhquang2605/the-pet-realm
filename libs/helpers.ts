35bbe2db36720d987e733405adef1308c4805312
anhquang2605 <anhquang2605@gmail.com>
2026-04-23 17:50:11 -0700

    added debounce

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -32,3 +32,16 @@ export const initializeErrorMessages = (fields: string[]): Record<string, { mess
     });
     return errorMessages;
 }
+
+export const debounce = <T extends (...args: any[]) => void>  (func: T, delay: number) => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+
+    return (...args: Parameters<T>) => {
+        if(timeoutId){
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func(...args);
+        }, delay);
+    }
+}
