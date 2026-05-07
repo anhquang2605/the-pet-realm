288172e7988c5787db5be803742c36013c23a70e
Anh Quang Chu <anhquang2605@gmail.com>
2026-05-07 10:40:45 -0700

    added link

--- components/universals/search-suggestion/suggestion-box/index.tsx
+++ components/universals/search-suggestion/suggestion-box/index.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import style from './suggestion-box.module.css';
 import { Order, ShopSuggestion } from '../../../../types/order';
+import Link from 'next/link';
 
 type SuggestionBoxProps = {
     suggestion: ShopSuggestion;
@@ -9,19 +10,16 @@ type SuggestionBoxProps = {
 const SuggestionBox: React.FC<SuggestionBoxProps> = ({
     suggestion
 }) => {
-    const handleClick = () => {
-        const path = `order/${suggestion.id}`;
-        
-    }
+    const path = `order/${suggestion.id}`;
     useEffect(() => {
 
     }, []);
 
     return (
-        <div onClick={handleClick} className={style['suggestion-box']}>
+        <Link href={path} className={style['suggestion-box']}>
             <img className={style['suggestion-box__image']} src={suggestion.image || '/asset/images/dogpet.webp'} alt={suggestion.name} />
             <p className={style['suggestion-box__name']}>{suggestion.name}</p>
-        </div>
+        </Link>
     );
 };
 
