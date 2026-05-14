52ef368042f42b5d0765e42f1deafdcc01029c05
Anh Quang Chu <anhquang2605@gmail.com>
2026-05-14 11:16:17 -0700

    trying to add image

--- components/sections/order-components/order-images/index.tsx
+++ components/sections/order-components/order-images/index.tsx
@@ -1,17 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import style from './order-images.module.css';
+import { useOrderContext } from '../useOrderContext';
 
 type OrderImagesProps = Record<string, never>;
 
 const OrderImages: React.FC<OrderImagesProps> = ({}) => {
+    const { order } = useOrderContext();
+
     useEffect(() => {
 
     }, []);
 
     return (
-        <div className={style['order-images']}>
-            OrderImages
-        </div>
+        order && 
+        <section className={style['order-images']}>
+            <img className={style['order-image']} src={order.imageUrls[0]} alt={order.name} />
+            
+        </section>
     );
 };
 
