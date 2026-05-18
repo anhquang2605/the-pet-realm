48b57411dad9e9c1acc1f0235da82e756d49b0ec
Anh Quang Chu <anhquang2605@gmail.com>
2026-05-18 12:22:25 -0700

    setting current image with one click

--- components/sections/order-components/order-images/image-slides/index.tsx
+++ components/sections/order-components/order-images/image-slides/index.tsx
@@ -3,18 +3,21 @@ import style from './image-slides.module.css';
 
 type ImageSlidesProps = {
     images: string[];
+    setCurrentImage?: (image: string) => void;
 };
 
-const ImageSlides: React.FC<ImageSlidesProps> = ({ images }) => {
+const ImageSlides: React.FC<ImageSlidesProps> = ({ images, setCurrentImage }) => {
     useEffect(() => {
 
     }, []);
-
+    const handleImageClick = (image: string) => {
+        setCurrentImage && setCurrentImage(image);
+    }
     return (
         <div className={style['image-slides']}>
             {
                 images.map((image, index) =>  (
-                    <img key={index} className={style['image-slides__image']} src={image} alt={`Slide ${index + 1}`} />
+                    <img key={index} className={style['image-slides__image']} src={image} alt={`Slide ${index + 1}`} onClick={() => handleImageClick(image)} />
                 ))  
                 
 
--- components/sections/order-components/order-images/index.tsx
+++ components/sections/order-components/order-images/index.tsx
@@ -8,18 +8,20 @@ type OrderImagesProps = Record<string, never>;
 
 const OrderImages: React.FC<OrderImagesProps> = ({}) => {
     const { order } = useOrderContext();
-
+    const [currentImage, setCurrentImage] = useState<string | null>(null);
     useEffect(() => {
-        console.log('Order in OrderImages:', order);
+            if (order && order.imageUrls.length > 0) {
+                setCurrentImage(order.imageUrls[0]);
+            }
     }, []);
 
     return (
         order && 
         <section className={style['order-images']}>
             {order.imageUrls.length > 1 && (
-                <ImageSlides images={order.imageUrls} />
+                <ImageSlides setCurrentImage={setCurrentImage} images={order.imageUrls} />
             )}
-            <img className={style['order-image']} src={order.imageUrls[0]} alt={order.name} />
+            <img className={style['order-image']} src={currentImage || order.imageUrls[0]} alt={order.name} />
               
         </section>
     );
