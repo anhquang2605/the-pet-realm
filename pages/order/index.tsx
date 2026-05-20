8d0bde6f330208f7c9a25015a0e962a28a435e30
Anh Quang Chu <anhquang2605@gmail.com>
2026-05-20 12:12:27 -0700

    working on orderPage

--- pages/order/index.tsx
+++ pages/order/index.tsx
@@ -1,14 +1,8 @@
 import { useState, useEffect, useRef}  from 'react';
 import { useSearchParams } from 'next/navigation';
 //get params from url
-import { useRouter } from 'next/router';
-import { fetchFromGetAPI } from '../../libs/api-interactions';
-import { RawOrder } from '../../types/order';
 import style from '../page-styles/order.module.css';
 import { OrderProvider } from '../../components/sections/order-components/useOrderContext';
-import OrderInfos from '../../components/sections/order-components/order-infos';
-import OrderImages from '../../components/sections/order-components/order-images';
-import OrderButtons from '../../components/sections/order-components/order-buttons';
 import OrderPageSectionSwitcher from '../../components/sections/order-components/order-page-section-switcher';
 export default function OrdersPage( ) {
     const searchParams = useSearchParams();
