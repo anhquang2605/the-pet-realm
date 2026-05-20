0e7def43525f8e66d5b69ea1f33189825809b23a
Anh Quang Chu <anhquang2605@gmail.com>
2026-05-20 11:55:42 -0700

    added backbones

--- components/sections/order-components/order-page-section-switcher/index.tsx
+++ components/sections/order-components/order-page-section-switcher/index.tsx
@@ -1,16 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import style from './order-page-section-switcher.module.css';
 
-type OrderPageSectionSwitcherProps = Record<string, never>;
+type OrderPageSectionSwitcherProps = {
+    sectionName: string;
+};
 
-const OrderPageSectionSwitcher: React.FC<OrderPageSectionSwitcherProps> = ({}) => {
+const OrderPageSectionSwitcher: React.FC<OrderPageSectionSwitcherProps> = ({ sectionName }) => {
     useEffect(() => {
 
     }, []);
-
+    const getSectionComponent = () => {
+        switch (sectionName) {
+            case 'details':
+                return <div>OrderDetails</div>;
+            case 'payment':
+                return <div>OrderPayment</div>;
+            default:
+                return null;
+        }
+    }
     return (
         <div className={style['order-page-section-switcher']}>
-            OrderPageSectionSwitcher
+            {getSectionComponent()}
         </div>
     );
 };
