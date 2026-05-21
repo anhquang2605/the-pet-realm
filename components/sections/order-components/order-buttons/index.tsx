ec99481c292f902743e52e25d6244ef887dc6e7a
Anh Quang Chu <anhquang2605@gmail.com>
2026-05-21 09:58:24 -0700

    added payment section

--- components/sections/order-components/order-buttons/index.tsx
+++ components/sections/order-components/order-buttons/index.tsx
@@ -1,16 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import style from './order-buttons.module.css';
+import { useOrderContext } from '../useOrderContext';
+import ActionButton from '../../../universals/buttons/action-button/action-button';
 
 type OrderButtonsProps = Record<string, never>;
 
 const OrderButtons: React.FC<OrderButtonsProps> = ({}) => {
+    const {sectionName, setSectionName} = useOrderContext();
     useEffect(() => {
 
     }, []);
 
     return (
         <div className={style['order-buttons']}>
-            OrderButtons
+            <ActionButton type='main' onClick={() => setSectionName('paymnent')} title='Order'/>
         </div>
     );
 };
