f8b469b304f503744ff1392082ad5b5605d10e73
Anh Quang Chu <anhquang2605@gmail.com>
2026-05-21 10:05:52 -0700

    added back button

--- components/sections/order-components/ORDER-SECTIONS/order-payment/index.tsx
+++ components/sections/order-components/ORDER-SECTIONS/order-payment/index.tsx
@@ -1,15 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import style from './order-payment.module.css';
+import { IoReturnUpBack } from "react-icons/io5";
+import { useOrderContext } from '../../useOrderContext';
 
 type OrderPaymentProps = Record<string, never>;
 
 const OrderPayment: React.FC<OrderPaymentProps> = ({}) => {
+    const {setSectionName} = useOrderContext();
     useEffect(() => {
 
     }, []);
+    const handleBackClick = () => {
+        setSectionName('details');
+    }
 
     return (
         <div className={style['order-payment']}>
+            <button><IoReturnUpBack />Back</button>
             OrderPayment
         </div>
     );
