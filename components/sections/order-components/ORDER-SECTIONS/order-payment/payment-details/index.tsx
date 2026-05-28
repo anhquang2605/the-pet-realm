f97305d0d11102d32b0055173243945f23d5db16
Anh Quang Chu <anhquang2605@gmail.com>
2026-05-28 11:15:23 -0700

    added on change

--- components/sections/order-components/ORDER-SECTIONS/order-payment/payment-details/index.tsx
+++ components/sections/order-components/ORDER-SECTIONS/order-payment/payment-details/index.tsx
@@ -6,7 +6,7 @@ import { Payments } from '../../../../../../types/payment';
 type PaymentDetailsProps = Record<string, never>;
 
 const PaymentDetails: React.FC<PaymentDetailsProps> = ({}) => {
-    const {payment} = useOrderContext();
+    const {payment, setPayment} = useOrderContext();
 
     useEffect(() => {
 
@@ -18,40 +18,40 @@ const PaymentDetails: React.FC<PaymentDetailsProps> = ({}) => {
                 <div className={style['payment-info']}>
                     <div className={style['payment-info-item']}>
                         <label className={style['payment-info-label']}>Card Number:</label> 
-                        <input  type="text" value={payment.cardNumber || ''} readOnly />
+                        <input className={style['payment-info-input']} onChange={(e) => setPayment(prev => ({...prev, cardNumber: e.target.value}))}  type="text" value={payment.cardNumber || ''} readOnly />
                     </div>
                     <div className={style['payment-info-item']}>
                         <label className={style['payment-info-label']}>Expiration Date:</label> 
-                        <input type="text" value={payment.expiryDate || ''} readOnly />
+                        <input className={style['payment-info-input']} onChange={(e) => setPayment(prev => ({...prev, expiryDate: e.target.value}))} type="text" value={payment.expiryDate || ''} readOnly />
                     </div>
                     <div className={style['payment-info-item']}>
                         <label className={style['payment-info-label']}>CVV:</label> 
-                        <input type="text" value={payment.cvv || ''} readOnly />
+                        <input className={style['payment-info-input']} onChange={(e) => setPayment(prev => ({...prev, cvv: e.target.value}))} type="text" value={payment.cvv || ''} readOnly />
                     </div>
                     {/* payment card holder name, billing address, city, state, postal code, country */}
                     <div className={style['payment-info-item']}>
                         <label className={style['payment-info-label']}  >Card Holder Name:</label> 
-                        <input type="text" value={payment.cardHolderName || ''} readOnly />
+                        <input className={style['payment-info-input']} onChange={(e) => setPayment(prev => ({...prev, cardHolderName: e.target.value}))} type="text" value={payment.cardHolderName || ''} readOnly />
                     </div>    
                     <div className={style['payment-info-item']}>
                         <label className={style['payment-info-label']}>Billing Address:</label> 
-                        <input type="text" value={payment.billingAddress1 || ''} readOnly />
+                        <input className={style['payment-info-input']} onChange={(e) => setPayment(prev => ({...prev, billingAddress1: e.target.value}))} type="text" value={payment.billingAddress1 || ''} readOnly />
                     </div>    
                     <div className={style['payment-info-item']}>    
                         <label className={style['payment-info-label']}>City:</label>    
-                        <input type="text" value={payment.city || ''} readOnly />
+                        <input className={style['payment-info-input']} onChange={(e) => setPayment(prev => ({...prev, city: e.target.value}))} type="text" value={payment.city || ''} readOnly />
                     </div>    
                     <div className={style['payment-info-item']}>
                         <label className={style['payment-info-label']}>State:</label> 
-                        <input type="text" value={payment.state || ''} readOnly />
+                        <input className={style['payment-info-input']} onChange={(e) => setPayment(prev => ({...prev, state: e.target.value}))} type="text" value={payment.state || ''} readOnly />
                     </div>    
                     <div className={style['payment-info-item']}>
                         <label className={style['payment-info-label']}>Postal Code:</label> 
-                        <input type="text" value={payment.postalCode || ''} readOnly />
+                        <input className={style['payment-info-input']} onChange={(e) => setPayment(prev => ({...prev, postalCode: e.target.value}))} type="text" value={payment.postalCode || ''} readOnly />
                     </div>    
                     <div className={style['payment-info-item']}>
                         <label className={style['payment-info-label']}>Country:</label> 
-                        <input type="text" value={payment.country || ''} readOnly />
+                        <input className={style['payment-info-input']} onChange={(e) => setPayment(prev => ({...prev, country: e.target.value}))} type="text" value={payment.country || ''} readOnly />
                     </div>
                 </div>
         </div>
