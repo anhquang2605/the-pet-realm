d761c403b7badd4968cd8b3b9a797ed0b65220fa
Anh Quang Chu <anhquang2605@gmail.com>
2026-05-28 11:11:15 -0700

    added the components

--- components/sections/order-components/ORDER-SECTIONS/order-payment/index.tsx
+++ components/sections/order-components/ORDER-SECTIONS/order-payment/index.tsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import style from './order-payment.module.css';
 import { IoReturnUpBack } from "react-icons/io5";
 import { useOrderContext } from '../../useOrderContext';
+import PaymentDetails from './payment-details';
+import Shipment from './shipment';
 
 type OrderPaymentProps = Record<string, never>;
 
@@ -18,6 +20,8 @@ const OrderPayment: React.FC<OrderPaymentProps> = ({}) => {
         <div className={style['order-payment']}>
             <button className={style['back-button']} onClick={handleBackClick}><IoReturnUpBack />Back</button>
             <h3 className={style['title']}>Payment</h3>
+            <PaymentDetails />
+            <Shipment setShipping={() => {}} />
         </div>
     );
 };
