e9f6d45c213d75d8adeaca6da3e741de3cb1027b
Anh Quang Chu <anhquang2605@gmail.com>
2026-06-01 14:32:49 -0700

    added content

--- components/universals/collapsable/collapsable-item/index.tsx
+++ components/universals/collapsable/collapsable-item/index.tsx
@@ -3,17 +3,21 @@ import style from './collapsable-item.module.css';
 
 type CollapsableItemProps = {
     isActive: boolean;
-    children: React.ReactNode
+    children: React.ReactNode;
+    title: string
 };
 
-const CollapsableItem: React.FC<CollapsableItemProps> = ({isActive, children}) => {
+const CollapsableItem: React.FC<CollapsableItemProps> = ({isActive, children, title = ''}) => {
     useEffect(() => {
 
     }, []);
 
     return (
         <div className={style['collapsable-item'] + (isActive ? ' ' + style['active'] : '')}>
-            {children}
+            <h3 className={style['title']}>{title}</h3>
+            <div className={style.content}>
+                {children}
+            </div>
         </div>
     );
 };
--- components/universals/collapsable/index.tsx
+++ components/universals/collapsable/index.tsx
@@ -4,16 +4,17 @@ import CollapsableItem from './collapsable-item';
 
 type CollapsableProps = {
     currentSection: number;//based on index of items
-    items: React.ReactNode[]
+    items: React.ReactNode[];
+    titles: string[]
 };
 
-const Collapsable: React.FC<CollapsableProps> = ({currentSection, items}) => {
+const Collapsable: React.FC<CollapsableProps> = ({currentSection, items = [], titles}) => {
     useEffect(() => {
 
     }, []);
     const generateContent = () => {
         return items.map((item, index) => (
-            <CollapsableItem key={index} isActive={index === currentSection}>
+            <CollapsableItem title={titles[index]} key={index} isActive={index === currentSection}>
                 {item}
             </CollapsableItem>
         ));
