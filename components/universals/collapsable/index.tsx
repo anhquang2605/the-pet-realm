c734d6ea98ff672d57aa142e78c84b4bb1167a54
Anh Quang Chu <anhquang2605@gmail.com>
2026-06-01 14:27:23 -0700

    incorporate current section

--- components/universals/collapsable/index.tsx
+++ components/universals/collapsable/index.tsx
@@ -1,13 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import style from './collapsable.module.css';
 
-type CollapsableProps = Record<string, never>;
+type CollapsableProps = {
+    currentSection: number;//based on index of items
+    items: React.ReactNode[]
+};
 
-const Collapsable: React.FC<CollapsableProps> = ({}) => {
+const Collapsable: React.FC<CollapsableProps> = ({currentSection, items}) => {
     useEffect(() => {
 
     }, []);
-
+    const generateContent = () => {
+        return items.map((item, index) => (
+            <div key={index} className={style['collapsable-item'] + (index === currentSection ? ' ' + style['active'] : '')}>
+                {item}
+            </div>
+        ));
+    }
     return (
         <div className={style['collapsable']}>
             Collapsable
