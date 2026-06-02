ec7814af775957cedb835523833b60fa0a5ce6d2
Anh Quang Chu <anhquang2605@gmail.com>
2026-06-02 10:37:27 -0700

    ideas flown

--- components/universals/collapsable/index.tsx
+++ components/universals/collapsable/index.tsx
@@ -2,10 +2,16 @@ import React, {useState, useEffect} from 'react';
 import style from './collapsable.module.css';
 import CollapsableItem from './collapsable-item';
 
+//map type with string as key and ReactNode as value
+export type FilledContent = {
+    [key: string]: React.ReactNode;
+};
+
 type CollapsableProps = {
     currentSection: number;//based on index of items
     items: React.ReactNode[];
-    titles: string[]
+    titles: string[];
+    filledContent: FilledContent;
 };
 
 const Collapsable: React.FC<CollapsableProps> = ({currentSection, items = [], titles}) => {
