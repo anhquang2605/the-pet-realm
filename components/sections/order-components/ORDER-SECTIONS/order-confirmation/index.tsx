13c08ef6fcfd774a2220459733ba24f85d64a3ad
Anh Quang Chu <anhquang2605@gmail.com>
2026-06-25 11:28:31 -0700

    working on order confirmation

--- components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
+++ components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
@@ -1,17 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import style from './order-confirmation.module.css';
-
+import { useOrderContext } from '../../useOrderContext';
 type OrderConfirmationProps = Record<string, never>;
 
 const OrderConfirmation: React.FC<OrderConfirmationProps> = ({}) => {
+    const {order, payment, shipping} = useOrderContext();
     useEffect(() => {
 
     }, []);
 
     return (
-        <div className={style['order-confirmation']}>
-            OrderConfirmation
-        </div>
+        <section className={style['order-confirmation']}>
+        </section>
     );
 };
 
