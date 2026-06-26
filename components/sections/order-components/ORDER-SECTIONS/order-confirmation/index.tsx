5a00826e0525dacaa7459db8ec541cfbc3dcb01b
Anh Quang Chu <anhquang2605@gmail.com>
2026-06-26 13:23:37 -0700

    added

--- components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
+++ components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import style from './order-confirmation.module.css';
 import { useOrderContext } from '../../useOrderContext';
+import FieldsDisplayer from '../../../../universals/fields-displayer';
 type OrderConfirmationProps = Record<string, never>;
 
 const OrderConfirmation: React.FC<OrderConfirmationProps> = ({}) => {
@@ -13,7 +14,9 @@ const OrderConfirmation: React.FC<OrderConfirmationProps> = ({}) => {
         <section className={style['order-confirmation']}>
             <h2 className={style['order-confirmation__title']}>We have received your order</h2>
             <p className={style['order-confirmation__message']}>Thank you for your purchase! Your order has been successfully placed. You will receive an email confirmation shortly with the details of your order and how to access it.</p>
-            
+            {order && <FieldsDisplayer items={[order]} sectionTitle="Order Details" />}
+            {payment && <FieldsDisplayer items={[payment]} sectionTitle="Payment Details" />}
+            {shipping && <FieldsDisplayer items={[shipping]} sectionTitle="Shipping Details" />}
         </section>
     );
 };
