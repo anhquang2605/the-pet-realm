5ad3870fc7b19d919080498da3a0e4c6387a3041
Anh Quang Chu <anhquang2605@gmail.com>
2026-06-30 11:05:47 -0700

    added order confirmation

--- components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
+++ components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
@@ -14,7 +14,7 @@ const OrderConfirmation: React.FC<OrderConfirmationProps> = ({}) => {
         <section className={style['order-confirmation']}>
             <h2 className={style['order-confirmation__title']}>We have received your order</h2>
             <p className={style['order-confirmation__message']}>Thank you for your purchase! Your order has been successfully placed. You will receive an email confirmation shortly with the details of your order and how to access it.</p>
-            {order && <FieldsDisplayer items={[order]} sectionTitle="Order Details" />}
+            {order && <FieldsDisplayer items={[order]} imageUrl={order.imageUrls[0]} sectionTitle="Order Details" />}
             {payment && <FieldsDisplayer items={[payment]} sectionTitle="Payment Details" />}
             {shipping && <FieldsDisplayer items={[shipping]} sectionTitle="Shipping Details" />}
             <p className={style['order-confirmation__message']}>If you have any questions or need assistance, please contact our support team.</p>
