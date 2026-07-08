c8b0828ba891e309650f2e50cef5d2d1ff7ff307
Anh Quang Chu <anhquang2605@gmail.com>
2026-07-08 10:46:25 -0700

    removed the static

--- components/sections/order-components/useOrderContext.tsx
+++ components/sections/order-components/useOrderContext.tsx
@@ -41,7 +41,7 @@ export const OrderContext = createContext<OrderContextType | null>(null);
 export const OrderProvider: React.FC<OrderProviderProps> = ({ children, id }) => {
     const [order, setOrder] = useState<RawOrder | null>(null);
     const [apiStatus, setApiStatus] = useState<'idle' | 'loading' | 'error' | 'success'>('idle');
-    const [sectionName, setSectionName] = useState<string>('confirmation');
+    const [sectionName, setSectionName] = useState<string>('details');
     const [filledContent, setFilledContent] = useState<{ [key: string]: boolean; }>({});
     const [currentFormStage, setCurrentFormStage] = useState<number>(1);
     const [orderSummary, setOrderSummary] = useState<OrderSummary | null>(null);
