ba6ab7ef478c0569b80f4713ab03ca6df60fc3f7
Anh Quang Chu <anhquang2605@gmail.com>
2026-07-14 10:23:21 -0700

    insert id

--- pages/api/payments/index.ts
+++ pages/api/payments/index.ts
@@ -5,7 +5,20 @@ import { getCollectionFromDB } from '../../../libs/db-interactions';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const collection = await getCollectionFromDB("payments");
     if(!collection) return res.status(500).json({ message: "Database connection error" });
-    if(req.method !== "GET") return res.status(405).json({ message: "Method not allowed" });
-    const orders = await collection.find({}).toArray();
-    res.status(200).json(orders);
+    switch(req.method) {
+        case "GET":
+            const payments = await collection.find({}).toArray();
+            res.status(200).json(payments);
+            break;
+        case "POST":
+            const payment = await collection.insertOne(req.body);
+            //get id of the inserted payment
+            const id = payment.insertedId.toString();
+            res.status(200).json({ id });
+            break;
+        default:
+            res.status(405).json({ message: "Method not allowed" });
+    
+    }
+
 }
