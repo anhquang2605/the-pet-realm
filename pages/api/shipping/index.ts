4fd1c847e02ccae502028273533bd9fef4df13eb
Anh Quang Chu <anhquang2605@gmail.com>
2026-07-14 10:29:16 -0700

    getting the ids

--- pages/api/shipping/index.ts
+++ pages/api/shipping/index.ts
@@ -5,6 +5,20 @@ import { ObjectId } from 'mongodb';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const collection = await getCollectionFromDB("shipping");
     if(!collection) return res.status(500).json({ message: "Database connection error" });
+    switch(req.method) {
+        case "GET":
+            const shippingData = await collection.find({}).toArray();
+            res.status(200).json(shippingData);
+            break;    
+        case "POST":
+            const returnedData = await collection.insertOne(req.body);
+            //get id of the inserted payment
+            const id = returnedData.insertedId.toString();
+            res.status(200).json({ id });
+            break;
+        default:
+            res.status(405).json({ message: "Method not allowed" });
+    }
     if(req.method !== "GET") return res.status(405).json({ message: "Method not allowed" });
     const shippingData = await collection.find({}).toArray();
     res.status(200).json(shippingData);
