ec9282c7b8fb00b3ce38447b3645ede6025bbf52
anhquang2605 <anhquang2605@gmail.com>
2026-08-03 18:37:55 -0700

    waiting on the finish

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
-import './../pages/globals.css';
+
 import Layout from "../components/sections/layout/layout";
+
 
 export default function Document({ Component, pageProps } : AppProps) {
   return (
