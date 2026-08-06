50cbd1b834bf0418f17f815c2c4ffb3334503ed9
Anh Quang Chu <anhquang2605@gmail.com>
2026-08-06 10:30:53 -0700

    changed

--- components/sections/order-components/ORDER-SECTIONS/order-payment/index.tsx
+++ components/sections/order-components/ORDER-SECTIONS/order-payment/index.tsx
@@ -28,10 +28,7 @@ const OrderPayment: React.FC<OrderPaymentProps> = ({}) => {
     }
     async function loadClientSecret() {
         const res = await getFromPOSTAPI('/stripe/create-checkout-session', { amount: 1000 }); // Replace with your actual API endpoint and parameters
-        console.log('Response from API:', res);
-        const data = await res.json();
-        console.log('Client Secret:', data.clientSecret);
-        setClientSecret(data.clientSecret);
+        setClientSecret(res.clientSecret);
     }
     useEffect(() => {
         loadClientSecret();
