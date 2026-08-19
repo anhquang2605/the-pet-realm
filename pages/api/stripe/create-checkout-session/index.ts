4a6e9b126d3131b1f2d1c252eb0bcde4fc4e0286
Anh Quang Chu <anhquang2605@gmail.com>
2026-08-18 19:48:12 -0700

    working on tax calculation

--- pages/api/stripe/create-checkout-session/index.ts
+++ pages/api/stripe/create-checkout-session/index.ts
@@ -1,18 +1,20 @@
 import Stripe from "stripe";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getFromPOSTAPI } from "../../../../libs/api-interactions";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 const URL = process.env.URL;
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { amount } = req.body;
-  
+    const { shipping } = req.body;
+    const orderAmount = await getOrderAmount(req.body.orderId);
+
     const session = await stripe.checkout.sessions.create({
        ui_mode: 'embedded_page',
     line_items: [
     {
       price_data: {
         currency: 'usd',
-        unit_amount: amount,
+        unit_amount: orderAmount,
         product_data: {
           name: 'Single Purchase',
         }
@@ -26,7 +28,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   res.status(200).json({ clientSecret: session.client_secret });
 }
 const getOrderAmount = async (orderId: string) => {
-      const order = await fetch(`${URL}/api/orders/${orderId}`);
-      const orderData = await order.json();
+    // Fetch the order from your database using the orderId
+    const orderData = await getFromPOSTAPI('/api/orders', { id: orderId, partialFields: ['amount'] });
       return orderData.amount;
 }
