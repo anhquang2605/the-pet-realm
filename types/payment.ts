25ab35f68935a59f1047ddf31932c1eace50b1dd
anhquang2605 <anhquang2605@gmail.com>
2026-08-19 09:32:41 -0700

    working on new type

--- types/payment.ts
+++ types/payment.ts
@@ -2,18 +2,12 @@ import { ObjectId } from "mongodb";
 
 export interface Payments {
    paymentIntentId: string; //stripe
-    expiryDate: string; // Format: MM/YY
-    city: string;
-    state: string;
-    postalCode: string;
-    country: string;
+    orderId: string | ObjectId;
 }
 export interface PaymentMethod extends Payments {
-    id: string | ObjectId;
     dateCreated: Date;
     last4Digits: string;
     methodType: 'credit' | 'debit' | 'paypal' | 'other'; // Example types
-    paymentIntentId: string; // For tracking payment intents if using a service like Stripe
 }
 export interface Shipping{
     recipientName: string;
