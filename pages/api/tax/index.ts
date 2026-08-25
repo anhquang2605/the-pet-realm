e305c562546d765f97506fd5ece35cb64f8f0e8e
anhquang2605 <anhquang2605@gmail.com>
2026-08-25 11:35:50 -0700

    fixed a mistake

--- pages/api/tax/index.ts
+++ pages/api/tax/index.ts
@@ -12,16 +12,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             res.status(200).json({ message: "GET request received" });
             break;
         case "POST":
-            const { shipping, productId } = req.body;
-            console.log("Received POST request with body:", req.body);
-            if (!shipping || !productId) {
+            const { shipping, productid } = req.body;
+            if (!shipping || !productid) {
                 return res.status(400).json({ message: "Missing required fields" });
             }
             const orderCollection = await getCollectionFromDB("orders");
             if (!orderCollection) {
                 return res.status(500).json({ message: "Database connection error" });
             }
-            const priceamount = await orderCollection.findOne({ _id: new ObjectId(productId) }, { projection: { price: 1, discount: 1 } });
+            const priceamount = await orderCollection.findOne({ _id: new ObjectId(productid) }, { projection: { price: 1, discount: 1 } });
             if (!priceamount) {
                 return res.status(404).json({ message: "Product not found" });
             }
