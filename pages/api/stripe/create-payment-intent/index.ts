a70ae43f968d622572cdac9af9f1177acd9ba3f7
anhquang2605 <anhquang2605@gmail.com>
2026-09-01 10:17:38 -0700

    fixed

--- pages/api/stripe/create-payment-intent/index.ts
+++ pages/api/stripe/create-payment-intent/index.ts
@@ -7,14 +7,13 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { tax, orderId } = req.body;
-    console.log(tax, orderId);
     if(!tax || !orderId) return res.status(400).json({ message: "Missing required fields" });
     const orderCollection = await getCollectionFromDB("orders");
     console.log(orderCollection);
     if(!orderCollection) return res.status(500).json({ message: "Database connection error" });
     const order = await orderCollection.findOne({ _id: new ObjectId(orderId) });
     if(!order) return res.status(404).json({ message: "Order not found" });
-    const amount = Math.round((order.amount *(1 - (order.discount || 0)) + tax) * 100); // Convert to cents 
+    const amount = Math.round((order.price *(1 - (order.discount || 0)) + tax) * 100); // Convert to cents 
     const paymentIntent = await stripe.paymentIntents.create({
         amount,
         currency: "usd",
