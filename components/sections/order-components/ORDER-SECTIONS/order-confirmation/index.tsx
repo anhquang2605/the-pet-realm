9b801c17983df6eba1fde5064017d6272fab6432
Anh Quang Chu <anhquang2605@gmail.com>
2026-09-07 21:26:36 -0700

    payment method

--- components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
+++ components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
@@ -6,7 +6,7 @@ import ActionButton from '../../../../universals/buttons/action-button/action-bu
 type OrderConfirmationProps = Record<string, never>;
 
 const OrderConfirmation: React.FC<OrderConfirmationProps> = ({}) => {
-    const {orderSummary, payment, shipping, order} = useOrderContext();
+    const {orderSummary, paymentMethod, shipping, order} = useOrderContext();
     useEffect(() => {
 
     }, []);
@@ -16,7 +16,7 @@ const OrderConfirmation: React.FC<OrderConfirmationProps> = ({}) => {
             <h2 className={style['order-confirmation__title']}>We have received your order</h2>
             <p className={style['order-confirmation__message']}>Thank you for your purchase! Your order has been successfully placed. You will receive an email confirmation shortly with the details of your order and how to access it.</p>
             {orderSummary && order && <FieldsDisplayer items={[orderSummary]} imageUrl={order.imageUrls[0]} sectionTitle="Order Details" />}
-            {payment && <FieldsDisplayer items={[payment]} sectionTitle="Payment Details" />}
+            {paymentMethod && <FieldsDisplayer items={[paymentMethod]} sectionTitle="Payment Details" />}
             {shipping && <FieldsDisplayer items={[shipping]} sectionTitle="Shipping Details" />}
             <p className={style['order-confirmation__message']}>If you have any questions or need assistance, please contact our support team.</p>
             <p className={style['order-confirmation__message']}>We look forward to serving you!</p>
