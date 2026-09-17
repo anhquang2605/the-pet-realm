30506a7f1871172b4f4a7ed4af1584a315c5fbcf
anhquang2605 <anhquang2605@gmail.com>
2026-09-16 17:33:23 -0700

    removed the unused information

--- components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
+++ components/sections/order-components/ORDER-SECTIONS/order-confirmation/index.tsx
@@ -52,12 +52,7 @@ const OrderConfirmation: React.FC<OrderConfirmationProps> = ({}) => {
         <section className={style['order-confirmation']}>
             <h2 className={style['order-confirmation__title']}>We have received your order</h2>
             <p className={style['order-confirmation__message']}>Thank you for your purchase! Your order has been successfully placed. You will receive an email confirmation shortly with the details of your order and how to access it.</p>
-            {orderSummary && order && <FieldsDisplayer items={[orderSummary]} imageUrl={order.imageUrls[0]} sectionTitle="Order Details" />}
-            {order && <FieldsDisplayer items={[{
-                _id: order._id,
-                name: order.name,
-                price: order.price
-            }]} sectionTitle="Order Information" />}
+            {orderSummary && order && <FieldsDisplayer items={[{...orderSummary, id: order._id}]} imageUrl={order.imageUrls[0]} sectionTitle="Order Details" />}S
             {shipping && <FieldsDisplayer items={[shipping]} sectionTitle="Shipping Details" />}
             <p className={style['order-confirmation__message']}>If you have any questions or need assistance, please contact our support team.</p>
             <p className={style['order-confirmation__message']}>We look forward to serving you!</p>
