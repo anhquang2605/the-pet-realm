59608054e014414215e3546919220545eb80db8b
Anh Quang Chu <anhquang2605@gmail.com>
2026-09-22 10:57:10 -0700

    removed unused mock data

--- local_data/mock-payment-data.ts
+++ local_data/mock-payment-data.ts
@@ -1,16 +1,5 @@
-import { Payments, Shipping } from '../types/payment';
-export const MOCK_PAYMENT: Payments = {
-    cardNumber: '1234567890123456',
-    cardHolderName: 'John Doe',
-    expiryDate: '12/25',
-    cvv: '123',
-    billingAddress1: '123 Main St',
-    billingAddress2: 'Apt 4B',
-    city: 'New York',
-    state: 'NY',
-    postalCode: '10001',
-    country: 'USA',
-}
+import { Shipping } from '../types/payment';
+
 export const MOCK_SHIPPING: Shipping = {
     recipientName: 'John Doe',
     email: 'jD4t3@example.com',
