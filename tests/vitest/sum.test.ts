fc1813d50db757cdcb1343b488c75733997aac01
Anh Quang Chu <anhquang2605@gmail.com>
2026-09-22 11:33:09 -0700

    working on testing

--- tests/vitest/sum.test.ts
+++ tests/vitest/sum.test.ts
@@ -1,7 +1,14 @@
-import { expect, test } from 'vitest'
+import { expect, test, describe } from 'vitest'
 import sum from './sum'
 
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+describe('sum', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
+
+  test('adds 1 + 4 to equal 5', () => {
+    expect(sum(1, 4)).toBe(5)
+  })
 })
+ 
