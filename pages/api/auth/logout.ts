3fbecbfad8e1b6f90af21b37924fd728c25c3612
anhquang2605 <anhquang2605@gmail.com>
2026-09-23 15:38:01 -0700

    removed unused items

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,9 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import bcrypt from 'bcryptjs';
 import { connectDB } from './../../../libs/mongoose';
 import Admin from './../../../models/Admin';
-import {SignJWT} from 'jose';
-const JWT_SECRET = process.env.NEXT_PUBLIC_JWT_SECRET || 'supersecretkey';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'DELETE') return res.status(405).end();
