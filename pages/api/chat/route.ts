da5ae4ce691dcc2cafca3e7dccf6d1ef3b8a0774
anhquang2605 <anhquang2605@gmail.com>
2026-09-23 15:37:29 -0700

    removed the unused

--- pages/api/chat/route.ts
+++ pages/api/chat/route.ts
@@ -1,50 +1,15 @@
 import { NextApiRequest, NextApiResponse  } from 'next';
-import {openai} from '../../../libs/openai';
 import { GoogleGenAI } from '@google/genai';
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
         return new Response('Method Not Allowed', { status: 405 });
     }
     const { message } = req.body as { message: string };
-   /*  const response = await openai.chat.completions.create({
-        model: 'gpt-5',
-        messages: messages,
-        stream: true,
-    });
-    const stream = new ReadableStream({
-        async start(controller) {
-            for await (const chunk of response) {
-                controller.enqueue(encodeText(
-                    chunk.choices[0].delta?.content || ''
-                ))
-            }
-            controller.close();
-        }
-    });
-            const encoder = new TextEncoder();
-     */
     if(!message || message.trim().length === 0) {
         return res.json({ error: "Message is required" });
     }
     const systemPrompt = "You are a helpful assistant.";
     try {
-/*         console.log("Generating completion for message:", message);
-
-    const completion = await openai.chat.completions.create({
-      model: "google/gemma-3-4b-it",
-      messages: [
-        {
-          "role": "system",
-          "content": systemPrompt,
-        },
-        {
-          "role": "user",
-          "content": message,
-        },
-      ],
-      temperature: 0.7,
-    });
-        const response = completion.choices[0].message.content; */
         const ai = new GoogleGenAI({
             apiKey: process.env.GEMINI_API_KEY || '',
         });
@@ -59,7 +24,3 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
       return res.json({ error: "Error generating completion" });
     }
 }
-
-/* function encodeText(text: string) {
-    return new TextEncoder().encode(text);
-} */
