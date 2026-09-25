0cd87a6d8d8af0327db362ff632b69901b3854e7
anhquang2605 <anhquang2605@gmail.com>
2026-09-25 10:38:34 -0700

    removed the unused console.log

--- pages/api/autocomplete-order-search/index.ts
+++ pages/api/autocomplete-order-search/index.ts
@@ -35,7 +35,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(200).json(results);
     })
     .catch((e) => {
-        console.log(e);
         res.status(500).json({ message: "Error fetching search results" });
     });
 }
